4ae7350e411967e86d303b9f82fc50dc49d21bd2
kouki-matsuura <kouki19991222@icloud.com>
2022-05-29 17:14:50 +0900

    first commit

---
+++ src/main/java/com/example/SecurityGetUserInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityGetUserInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityGetUserInfoApplication.class, args);
+	}
+
+}
