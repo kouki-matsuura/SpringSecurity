6918faf6860ffdef9562fb5363ed1f2816d72158
松浦　昂生 <kouki19991222@icloud.com>
2022-05-30 21:44:02 +0900

    [add]独自のユーザクラスの作成

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -3,20 +3,25 @@ package com.example.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@SuppressWarnings("deprecation")
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	@Qualifier("UserDetailsServiceImpl")
+	private UserDetailsService userDetailsService;
 	
 	 //パスワードエンコーダーのBean定義
     @Bean
@@ -85,11 +90,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
      // ログイン処理時のユーザー情報を、DBから取得する
-     auth.jdbcAuthentication()
+    /*auth.jdbcAuthentication()
               .dataSource(dataSource)
               .usersByUsernameQuery(USER_SQL) //ユーザーの取得
               .authoritiesByUsernameQuery(ROLE_SQL) //ロールの取得
-              .passwordEncoder(passwordEncoder()); //パスワードの復号
+              .passwordEncoder(passwordEncoder()); //パスワードの復号*/
+    	
+    	auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
         
 
     }
--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -6,10 +6,11 @@ import java.security.Principal;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.domain.model.AppUserDetails;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,7 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 public class HomeController {
 	 @GetMapping("/home")
 	    public String getHome(Model model,
-	            @AuthenticationPrincipal User user) {
+	            @AuthenticationPrincipal AppUserDetails user) {
 
 	        log.info("HomeController Start");
 
@@ -38,15 +39,14 @@ public class HomeController {
 
 	        //ログインユーザー情報の取得(その1)
 	        Authentication authentication = (Authentication) principal;
-	        User user1 = (User) authentication.getPrincipal();
+	        AppUserDetails user1 = (AppUserDetails) authentication.getPrincipal();
 	        log.info("user1: " + user1.toString());
 
 	        //ログインユーザー情報の取得(その2)
-	        User user2 = (User) SecurityContextHolder
-	                .getContext()
-	                .getAuthentication()
-	                .getPrincipal();
-
+	        AppUserDetails user2 = (AppUserDetails) SecurityContextHolder
+	        		.getContext()
+	        		.getAuthentication()
+	        		.getPrincipal();
 	        log.info("user2: " + user2.toString());
 
 	        return "home";
